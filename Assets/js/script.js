f407a16fdc1f03531fbd9d98c703fbc826173876
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-04-02 11:07:20 +1100

    addded submit value to buttons and as well as col value to each row

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,16 @@
-//Live date and time inbedded into the Jumbotron
+//Live date and time showing in Jumbotron
 
-function Timeupdate() {
+function LiveTime() {
 $("#currentDay").text(moment().format("LLLL"))
 }
+setInterval(LiveTime, 1000);
 
-setInterval(Timeupdate, 1000)
+//variables for each timeblock 9-5
+var Nine = moment().hour(9)
+
+console.log(Nine.format("h"));
+
+//Setting the start of day as 9am on any given day
+
+
+
