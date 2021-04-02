9ed8c3828e759f38f1e9e07354e820e43cd6d1da
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-04-02 12:14:53 +1100

    added console log for each timeblock to ensure proper function

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,7 +8,7 @@ setInterval(LiveTime, 1000);
 //variables for each timeblock 9-5
 var Nine = moment().hour(9).format("h")
 var Ten = moment().hour(10).format("h")
-var Eleve = moment().hour(11).format("h")
+var Eleven = moment().hour(11).format("h")
 var Twelve = moment().hour(12).format("h")
 var One = moment().hour(13).format("h")
 var Two = moment().hour(14).format("h")
@@ -16,7 +16,7 @@ var Three = moment().hour(15).format("h")
 var Four = moment().hour(16).format("h")
 var Five = moment().hour(17).format("h")
 
-console.log("")
+console.log(Nine, Ten, Eleven, Twelve, One, Two, Three, Four, Five)
 
 
 //Setting the start of day as 9am on any given day
