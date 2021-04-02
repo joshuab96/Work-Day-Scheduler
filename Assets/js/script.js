01e9351afc5195a3588786361ea743c813759b08
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-04-02 12:12:07 +1100

    assigned set hour times to each timeblock 9=5

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,11 +6,19 @@ $("#currentDay").text(moment().format("LLLL"))
 setInterval(LiveTime, 1000);
 
 //variables for each timeblock 9-5
-var Nine = moment().hour(9)
+var Nine = moment().hour(9).format("h")
+var Ten = moment().hour(10).format("h")
+var Eleve = moment().hour(11).format("h")
+var Twelve = moment().hour(12).format("h")
+var One = moment().hour(13).format("h")
+var Two = moment().hour(14).format("h")
+var Three = moment().hour(15).format("h")
+var Four = moment().hour(16).format("h")
+var Five = moment().hour(17).format("h")
 
-console.log(Nine.format("h"));
+console.log("")
+
 
 //Setting the start of day as 9am on any given day
-
 
 
