3f52dfbb861595c39bd74c78378339b55d33c649
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-04-11 08:54:17 +1000

    moved global selectors into for loop to fix issue with adding classes to each row

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,13 +10,6 @@ setInterval(LiveTime, 1000);
 
 mainEl = $(".container")
 
-//Creating timeblocks via a for loop
-
-rowEl = $("<div></div>").addClass("row time-block")
-hourEl = $("<div></div>").addClass("hour col")
-inputEl = $("<input></input>").addClass("form-control-plaintext description col")
-saveBtnEl = $("<div></div>").addClass("saveBtn button col submit d-flex h-100 ")
-iconEl = $("<i></i>").addClass("fas fa-save col justify-content-center align-self-center ")
 
 // Hours 9-5 with corresponding AM and PM displayed
 
@@ -24,10 +17,17 @@ var timeDesc = {
     hours:["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"],
     classHourid:["9","10","11","12","1","2","3","4","5"]
 }
-
+//Creating timeblocks via a for loop
 // to dynamically append 8 timeblocks 9-5 to html page
 
  for (i = 0; i < 10; i++) {
+
+    rowEl = $("<div></div>").addClass("row time-block")
+    hourEl = $("<div></div>").addClass("hour col")
+    inputEl = $("<input></input>").addClass("form-control-plaintext description col")
+    saveBtnEl = $("<div></div>").addClass("saveBtn button col submit d-flex h-100 ")
+    iconEl = $("<i></i>").addClass("fas fa-save col justify-content-center align-self-center ")
+
     $(mainEl).append(rowEl.clone());
     $(rowEl).append(hourEl, inputEl, saveBtnEl);
     $(saveBtnEl).append(iconEl);
