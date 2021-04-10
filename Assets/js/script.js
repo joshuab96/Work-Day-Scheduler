efdf287b24c313311681c59d4ed408a1242609de
Joshua-Rhymer Bernardo <joshua.bernardo@live.com>
2021-04-11 08:34:45 +1000

    minor changes and tweaks

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -21,7 +21,8 @@ iconEl = $("<i></i>").addClass("fas fa-save col justify-content-center align-sel
 // Hours 9-5 with corresponding AM and PM displayed
 
 var timeDesc = {
-    hours:["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
+    hours:["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"],
+    classHourid:["9","10","11","12","1","2","3","4","5"]
 }
 
 // to dynamically append 8 timeblocks 9-5 to html page
@@ -30,8 +31,11 @@ var timeDesc = {
     $(mainEl).append(rowEl.clone());
     $(rowEl).append(hourEl, inputEl, saveBtnEl);
     $(saveBtnEl).append(iconEl);
-    $(hourEl).text(timeDesc.hours[i])
+    $(hourEl).text(timeDesc.hours[i]);
+    $(inputEl).addClass(timeDesc.classHourid[i]);
     
 }
+
+// Displaying colour coded blocks in regards to hour time whether it is the past, preset or future, using grey, green or red.
 
 
